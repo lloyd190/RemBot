52e65f2ddc6bacfbfed7dbbe47e30edd59d81ab3
Unknown <lkuijs@student.scalda.nl>
2017-11-22 15:31:31 +0100

    Can read messages, module class added

--- RemDiscordBot/Program.cs
+++ RemDiscordBot/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,23 +15,68 @@ namespace RemDiscordBot
     {
         //TODO: move tokens and other server sided variables to a separate file later on
         private readonly String _token = "MzgxMDA0MjU3NTU5NzA3NjQ4.DPBsvw.kmbEujIW2IsvMXXOGJcJkaPHj_0";
+        private IServiceProvider _services;
         private DiscordSocketClient _socketClient;
+        private CommandService _commands;
         static void Main(string[] args) => new Program().MainAsync(args).GetAwaiter().GetResult();
 
         public async Task MainAsync(string[] args)
         {
-            _socketClient = new DiscordSocketClient();
-            _socketClient.Log += Log;
+            try
+            {
+                _socketClient = new DiscordSocketClient();
+                _socketClient.Log += Log;
 
-            await _socketClient.LoginAsync(TokenType.Bot, _token);
-            await _socketClient.StartAsync();
+                _commands = new CommandService();
 
-            await Task.Delay(-1);
+                _services = new ServiceCollection()
+                    .AddSingleton(_socketClient)
+                    .AddSingleton(_commands)
+                    .BuildServiceProvider();
+
+
+                await InstallCommandsAsync();
+                await _socketClient.LoginAsync(TokenType.Bot, _token);
+                await _socketClient.StartAsync();
+
+                await Task.Delay(-1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
         }
-            private Task Log(LogMessage msg)
+        public async Task InstallCommandsAsync()
+        {
+            _socketClient.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+            if (!message.HasMentionPrefix(_socketClient.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(_socketClient, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
             {
-                Console.WriteLine(msg.ToString());
+                await context.Channel.SendMessageAsync("Sorry? that doesn't make sense to me.");
+            }
+            if(result.IsSuccess)
+            {
+                Console.WriteLine("message received");
+            }
+        }
+
+        private Task Log(LogMessage message)
+            {
+                Console.WriteLine(message.ToString());
                 return Task.CompletedTask;
             }
         }

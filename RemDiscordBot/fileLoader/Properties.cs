b0c822dcf0be152675210da9cb3afe21f691970c
Unknown <lkuijs@student.scalda.nl>
2017-12-15 15:35:50 +0100

    getting regular expressions to work

--- RemDiscordBot/fileLoader/Properties.cs
+++ RemDiscordBot/fileLoader/Properties.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RemDiscordBot.fileLoader
@@ -16,11 +17,18 @@ namespace RemDiscordBot.fileLoader
             try
             {
                 StreamReader streamReader = new StreamReader(filePath);
-                Console.WriteLine(streamReader.ReadLine().ToString());
+                while (streamReader.ReadLine() != null)
+                {
+                    string line = streamReader.ReadLine();
+                    if (Regex.IsMatch(line, @"^"))
+                    {
+
+                    }
+                }
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                throw new ArgumentException("File was not found at given filePath");
+                throw e;
             }
         }
 
